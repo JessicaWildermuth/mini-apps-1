c2d624b190464cbd331012956bb36a4dbdf7f846
JessicaWildermuth <jwildermuth536@gmail.com>
2020-09-14 16:20:29 -0700

    Make player moves render to page

--- challenge_1/appRefactored.js
+++ challenge_1/appRefactored.js
@@ -1,12 +1,37 @@
 //want to use a 2d model to represent my game board
 //game board is empty to start
 //a zero in the array index indicates no X or O
+
+//MODEL
+
 var gameBoard = [
-  ['a', 0, 0],
-  [0, 'a', 0],
-  [0, 0, 'a'],
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
 ];
 
+var currentPlayer = 'X';
+
+var updateGameBoard = function(row, col) {
+  if (gameBoard[row][col] !== 0) {
+    return;
+  }
+  //update the gameboard with correct value
+  gameBoard[row][col] = currentPlayer;
+  //update current Player
+  if (currentPlayer === 'X') {
+    currentPlayer = 'O';
+  } else {
+    currentPlayer = 'X';
+  }
+  renderGameBoard();
+}
+
+var checkForWinner = function() {
+
+}
+
+//VIEW
 //have a function that renders the values that are in my gameboard array
 function renderGameBoard() {
   //for each element in gameboard,
@@ -24,5 +49,25 @@ function renderGameBoard() {
     }
   }
 }
+
+
+
+//CONTROLLER
+var gameBoxes = document.getElementsByClassName('gamebox');
+for (var i = 0; i < gameBoxes.length; i++) {
+  var gamebox = gameBoxes[i];
+  gamebox.addEventListener('click', function() {
+    //add the right value to gameboard model
+      //access the game model row and col
+      var id = this.id;
+      var row = Number(id[0]);
+      var col = Number(id[1]);
+    //update my game model
+    updateGameBoard(row, col);
+  });
+}
+//each gameboard element needs to be clickable
+//when I click, it add an X or and O to the gameboard model array
+//shows the current gameboard mopdel
 
 console.log(gameBoard);
