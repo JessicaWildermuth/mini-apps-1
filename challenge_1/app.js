74551274a494615cb8256c016b82a63f39430a47
JessicaWildermuth <jwildermuth536@gmail.com>
2020-09-14 12:00:45 -0700

    Adding X or O depending on count

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,32 +1,40 @@
+//select all boxes in game board
+var gameBoxes = document.getElementsByClassName('box');
 
-// each block should have an onclick ability where it adds an X or an O
-// game always starts with X
-// should maybe keep a count and if the count is even we add an X on click
-// if it is odd we add an O
-// after each onClick, we need to check for a winner
-// to check for a winner
-//   need to go over all the rows and columns and check if there are three in a row
-//   so if row1Col1 has an x/o we need to check if the rest of the row has an x/o the same col has an
-//   x/o or if next row col+ & next next row col++ has an x/o
-//if any of them do, we declare a winner and the game ends
+
+//will rely on a count so there needs to be a global variable of count
+var count = 0;
 
 
 //create an addListen event and click handler for all boxes (easy to grab using getElementsByClassName)
-
-
-var gameBoxes = document.getElementsByClassName('box');
-
+//need to map over the array of elements and add an onclick functionality to them all
 var addClickEvent = function(elements) {
   for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
-    console.log(element);
-    element.addEventListener('click', function() {
-      alert('You clicked me!');
+    //successfully added onclick functionality to each
+    //now need to change the function here to a define function that add a specific text to that box
+    //write a function that is passed in to the addEventListener
+    element.addEventListener('click',  function() {
+      //since the first play is always X - x is placed when the count is even
+      if (count % 2 == 0) {
+        //to add a X to the box, the box should probably have a text node (input box of some sort that can be dynamically changed)
+        var X = document.createTextNode('X');
+        console.log(element)
+        element.append.bind(this)(X);
+        count++;
+      } else {  //o is placed when the count is odd
+        var O = document.createTextNode('O');
+        element.append.bind(this)(O);
+        count++;
+      }
     });
   }
 }(gameBoxes);
 
+
+
+
+
+
 //successfuly grabbed all the boxes
 //now loop through the array of html elements and add an event listen to each
-console.log(addClickEvent);
-console.log(gameBoxes);
