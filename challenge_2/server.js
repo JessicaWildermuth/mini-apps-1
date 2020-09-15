5824ba05e80c70ad69bf40d5f99c05f701426351
JessicaWildermuth <jwildermuth536@gmail.com>
2020-09-15 10:25:32 -0700

    Create express server and serve up index.html

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app =  express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+  res.sendFile('/Users/jwildermuth/hackreactor/hrsf130-mini-apps-1/challenge_2/client/index.html')
+})
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+})
 
